11d5a5e096301613340319352c7693cd94ffb8be
𝓢𝓮𝓿𝓲𝓷𝓬𝓱💫 <ysevinchoy2197@gmail.com>
2025-11-12 18:12:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+#1-misol
+matn = input("Salom dunyo")
+print((matn.lower))
+
+#2-misol
+matn = input("PYTthoN DasTURlash")
+print((matn.upper()))
+
+#3-misol
+parol = input("Parol kiriting: ")
+if "admin" in parol:
+    print("Xavfsiz parol emas")
+else:
+    print("Parol qabul qilindi")
+
+#4-misol
+matn = input("  Salom dunyo  ")
+print((matn.strip))
+
+#5-misol
+email = input("email manzil kiriting: ")
+if "email" in email:
+    print("Tozalangan email")
+else:
+    print("Tozalanmagan email")
+
+6-misol
+matn = "Salom dunyo"
+print((matn.strip))
+
+#7-misol
+matn = "Salom dumyo"
+print((matn.strip))
